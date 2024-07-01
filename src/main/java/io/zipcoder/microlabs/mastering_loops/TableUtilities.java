acf0a3f174a5eb9f21b1e648a1daf0a2fc0546ef
Jon <jon@Zipcoders-MacBook-Pro-99.local>
2024-07-01 19:42:54 -0400

    Finished Lab

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,47 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        //    Generate a String representation of a multiplication table whose dimensions are 4 by 4
+        String s = "";
+
+        for(int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                s += String.format("%3d |", i * j);
+            }
+            s += "\n";
+        }
+        return s;
     }
+
+
+
+
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        //     Generate a String representation of a multiplication table whose dimensions are 9 by 9
+        String s = "";
+
+        for(int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                s += String.format("%3d |", i * j);
+            }
+            s += "\n";
+        }
+        return s;
     }
 
+
+
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        //      Given one integer, width, generate a String representation of a multiplication table whose dimensions are width by width
+        String s = "";
+
+        for(int i = 1; i <= tableSize; i++) {
+            for(int j = 1; j <= tableSize; j++) {
+                s += String.format("%3d |", i * j);
+            }
+            s += "\n";
+        }
+        return s;
     }
 }
